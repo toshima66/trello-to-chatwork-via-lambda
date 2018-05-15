686a3c11e56c77a5901528d8f23ae72c8bdc476b
toshima66 <t.oshima.66@gmail.com>
2018-05-15 11:21:20 +0900

    add receive trello notifications

--- handler.js
+++ handler.js
@@ -1,16 +1,100 @@
 'use strict';
 
+var querystring = require('querystring');
+var https = require('https');
+
+class Trello {
+  constructor(event) {
+    this.json = JSON.parse(event.body);
+    this.actionType = this.json.action.type;
+  }
+
+  isTarget() { return ('commentCard' === this.actionType); }
+};
+
+class Chatwork {
+  constructor(context, trello) {
+    this.context = context;
+    this.trello = trello;
+  }
+
+  notify() {
+    const t = this.trello;
+
+    if (!t.isTarget()) {
+      console.log('is not target');
+      return ;
+    }
+
+    console.log('is target');
+    console.log(t.json.action.data.text);
+
+    post();
+
+    return response();
+  }
+
+  post() {
+    var req = https.request(postOptions(), function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (body) {
+        console.log(body);
+        succeed();
+      });
+    }).on('error', function(e) {
+      error();
+    });
+
+    req.write(data);
+    req.end();
+  }
+
+  data() {
+    // TODO
+    // trello json to chatwork
+    // XXX が YYY にコメントしました
+    // xxxxxxxxxx
+    // xxxxxxxxxx
+//    var data = querystring.stringify({
+//        body: 'hoge121@example.com'
+//    });
+  }
+
+  postOptions() {
+    hostname: '${host}',
+    port: ${port},
+    path: '${path}',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(data()),
+      'X-ChatWorkToken': '${chatwork_token}'
+    }
+  }
+
+  succeed() {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'OK',
+        input: event,
+      }),
+    };
+  }
+
+  error() {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'NG',
+        input: event,
+      }),
+    };
+  }
+};
+
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  const t = new Trello(event);
+  const c = new Chatwork(context, t);
+  callback(null, c.notify());
 };
